bd64a880bd41b70024b26cd36fedd0c30cf93287
Kirim Lee <uthi1004@gmail.com>
2019-08-07 09:34:35 +0900

    scaffolding

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import { randome } from "./util";
+
+const rOne = randome(10);
+const rTwo = randome(20);
+
+console.log(`${rOne}, ${rTwo}`);
---
+++ src/js/util.js
@@ -0,0 +1,1 @@
+export const randome = max => Math.floor(Math.random() * max);
