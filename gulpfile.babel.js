a0f78766dc465196c8923b483488acdb74eb5f49
Kirim Lee <uthi1004@gmail.com>
2019-08-13 13:56:04 +0900

    pug task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,16 @@
 import gulp from "gulp";
+import pug from "gulp-pug";
+
+const routes = {
+  pug: {
+    src: "src/**/*.pug",
+    dest: "build"
+  }
+};
+export const buildHTML = () =>
+  gulp
+    .src(routes.pug.src)
+    .pipe(pug())
+    .pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series(buildHTML);
