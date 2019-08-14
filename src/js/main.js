fa7e4df7318cfba38eed68bac60f29249557a2b5
이기림(Kirim Lee)/FE개발팀/SKP <kirim@sk.com>
2019-08-14 10:42:48 +0900

    Update 2019-08-14T01:42:45.540Z

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import { randome } from "./util";
-
-const rOne = randome(10);
-const rTwo = randome(20);
-
-console.log(`${rOne}, ${rTwo}`);
--- src/js/util.js
+++
@@ -1,1 +0,0 @@
-export const randome = max => Math.floor(Math.random() * max);
